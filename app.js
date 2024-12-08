70c9e0039f6ad02121fd9ab1946d452e611dc08f
shivam kumar <nr750001@gmail.com>
2024-12-08 17:03:25 +0530

    yo

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+module.exports = server;
